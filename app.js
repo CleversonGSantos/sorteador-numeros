782a1c3912e4692de724b009a796683195d53f14
Cleverson G Santos <ioreyagami@gmail.com>
2025-03-11 21:29:11 -0300

    Adiciona validações para o sorteio de números e estiliza mensagens de erro

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ function sortear() {
     let numeroInicial = parseInt(document.getElementById('de').value);
     let numeroMaximo = parseInt(document.getElementById('ate').value);
 
+    let diferenca = numeroMaximo - numeroInicial + 1;
+
+    if (diferenca < 0) {
+        alteraElementoTexto('resultado', `O número inicial deve ser menor ou igual ao número máximo`, 'texto__erro');
+        alteraEstadoBotao('btn-reiniciar', true);
+        return;
+    }
+
+    if (quantidadeNumero > diferenca) {
+        alteraElementoTexto('resultado', `A quantidade de números deve ser menor ou igual a ${diferenca}`, 'texto__erro');
+        alteraEstadoBotao('btn-reiniciar', true);
+        return;
+    }
+
+
     let numerosSorteados = [];
     for (let i = 0; i < quantidadeNumero; i++) {
         do {
@@ -15,7 +30,7 @@ function sortear() {
     }
 
     alteraElementoTexto('resultado', `Números sorteados: ${numerosSorteados.join(', ')}`, 'texto__paragrafo');
-    alteraEstadoBotao('btn-reiniciar');
+    alteraEstadoBotao('btn-reiniciar', true);
 }   
   
 
@@ -26,6 +41,9 @@ function obterNumeroAleatorio(min, max) {
 function alteraElementoTexto(id, texto, css) {
     let campo = document.getElementById(id);
     campo.innerHTML = texto; // Atualiza o conteúdo do elemento
+    if (campo.classList.contains('texto__erro')) {
+        campo.classList.remove('texto__erro');
+    }
     campo.classList.add(css); // Garante que a classe seja mantida
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female');
 }
@@ -35,16 +53,16 @@ function reiniciar() {
     limpar('quantidade');
     limpar('de');
     limpar('ate');
-    alteraEstadoBotao('btn-reiniciar');
+    alteraEstadoBotao('btn-reiniciar', false);
 }
 
 function limpar(id) {
     document.getElementById(id).value = '';
 }
 
-function alteraEstadoBotao(id) {
+function alteraEstadoBotao(id, estado) {
     let botao = document.getElementById(id);
-    if (botao.classList.contains('container__botao-desabilitado')) {
+    if (estado) {
         botao.removeAttribute('disabled');
         botao.classList.add('container__botao');
         botao.classList.remove('container__botao-desabilitado');
